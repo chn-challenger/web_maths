a08ae39ef1c60068cdcf5bea9a8db4d94b7ae182
Joe Zhou <chn.challenger@gmail.com>
2015-11-24 18:08:01 +0000

    division question tests and implementation

--- generators/question.rb
+++ generators/question.rb
@@ -13,6 +13,7 @@ module Questions
     return _addition(integer_range,fraction_range) if operation == 'addition'
     return _subtraction(integer_range,fraction_range) if operation == 'subtract'
     return _multiplication(integer_range,fraction_range) if operation == 'multiply'
+    return _division(integer_range,fraction_range) if operation == 'divide'
   end
 
   private
@@ -20,8 +21,8 @@ module Questions
   def _addition(integer_range,fraction_range)
     fraction1 = random_fraction(integer_range,fraction_range)
     fraction2 = random_fraction(integer_range,fraction_range)
-    {operator: 'addition',fraction1: fraction1,fraction2: fraction2,
-      solution: fraction1.add(fraction2)}
+    {operator:'addition',fraction1:fraction1,fraction2:fraction2,
+      solution:fraction1.add(fraction2)}
   end
 
   def _subtraction(integer_range,fraction_range)
@@ -30,15 +31,22 @@ module Questions
       fraction2 = random_fraction(integer_range,fraction_range)
       break if fraction2 < fraction1
     end
-    {operator: 'subtract',fraction1: fraction1,fraction2: fraction2,
-      solution: fraction1.subtract(fraction2)}
+    {operator:'subtract',fraction1:fraction1,fraction2:fraction2,
+      solution:fraction1.subtract(fraction2)}
   end
 
   def _multiplication(integer_range,fraction_range)
     fraction1 = random_fraction((integer_range*0.6).to_i,(fraction_range*0.8).to_i)
     fraction2 = random_fraction((integer_range*0.6).to_i,(fraction_range*0.8).to_i)
-    {operator: 'multiply',fraction1: fraction1,fraction2: fraction2,
-      solution: fraction1.multiply(fraction2)}
+    {operator:'multiply',fraction1:fraction1,fraction2:fraction2,
+      solution:fraction1.multiply(fraction2)}
+  end
+
+  def _division(integer_range,fraction_range)
+    fraction1 = random_fraction((integer_range*0.6).to_i,(fraction_range*0.8).to_i)
+    fraction2 = random_fraction((integer_range*0.6).to_i,(fraction_range*0.8).to_i)
+    {operator:'divide',fraction1:fraction1,fraction2:fraction2,
+      solution:fraction1.divide(fraction2)}
   end
 
 end
--- spec/question_spec.rb
+++ spec/question_spec.rb
@@ -114,32 +114,32 @@ describe Questions do
       end
     end
 
-    # context 'it can generates random fraction addition questions' do
-    #   shared_context 'example addition question' do
-    #     before(:all) do
-    #       srand(100)
-    #       dummy_class = Class.new{include Questions}.new
-    #       @question = dummy_class.fraction_question
-    #     end
-    #   end
-    #
-    #   include_context 'example addition question'
-    #
-    #   it 'has operator of addition' do
-    #     expect(@question[:operator]).to eq 'addition'
-    #   end
-    #
-    #   it 'generates a fraction for the addition' do
-    #     expect(@question[:fraction1]).to eq Fraction.new(8,2,5)
-    #   end
-    #
-    #   it 'generates another fraction for the addition' do
-    #     expect(@question[:fraction2]).to eq Fraction.new(7,8,9)
-    #   end
-    #
-    #   it 'generates solution to the question' do
-    #     expect(@question[:solution]).to eq Fraction.new(16,13,45)
-    #   end
-    # end
+    context 'it can generates random fraction division questions' do
+      shared_context 'example division question' do
+        before(:all) do
+          srand(400)
+          dummy_class = Class.new{include Questions}.new
+          @question = dummy_class.fraction_question('divide')
+        end
+      end
+
+      include_context 'example division question'
+
+      it 'has operator of divide' do
+        expect(@question[:operator]).to eq 'divide'
+      end
+
+      it 'generates a fraction for the division' do
+        expect(@question[:fraction1]).to eq Fraction.new(4,1,4)
+      end
+
+      it 'generates another fraction for the division' do
+        expect(@question[:fraction2]).to eq Fraction.new(4,4,5)
+      end
+
+      it 'generates solution to the question' do
+        expect(@question[:solution]).to eq Fraction.new(0,85,96)
+      end
+    end
   end
 end

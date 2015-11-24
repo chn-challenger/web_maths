8b5f4505d0fd7e01baff2b37af8d7530d8d67441
chn.challenger <chn.challenger@gmail.com>
2015-11-24 10:15:06 +0000

    texstudio settings

--- test_stuff.rb
+++ test_stuff.rb
@@ -8,9 +8,24 @@ p f1 == f2
 
 
 
-
-
-
+/Library/TeX/texbin/latex -src -interaction=nonstopmode %.tex
+/Library/TeX/texbin/pdflatex -synctex=1 -interaction=nonstopmode %.tex
+/Library/TeX/texbin/xelatex -synctex=1 -interaction=nonstopmode %.tex
+/Library/TeX/texbin/lualatex -synctex=1 -interaction=nonstopmode %.tex
+/Library/TeX/texbin/dvips -o %.ps %.dvi
+/Library/TeX/texbin/dvipng -T tight -D 120 %.dvi
+/Library/TeX/local/bin/ps2pdf %.ps
+/Library/TeX/local/bin/dvipdf %.dvi
+/Library/TeX/texbin/bibtex %.aux
+/Library/TeX/texbin/bibtex8 %.aux
+/Library/TeX/texbin/biber %
+/Library/TeX/texbin/makeindex %.idx
+/Library/TeX/texbin/texindy %.idx
+sh -c "PATH=$PATH:/Library/TeX/texbin/; makeglossaries %"
+/Library/TeX/texbin/mpost -interaction=nonstopmode ?me)
+/Library/TeX/texbin/asy ?m*.asy
+/usr/local/bin/gs "?am.ps"
+latexmk -pdf -silent -latexoption="-synctex=1" %
 
 
 

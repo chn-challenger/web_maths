be21427458100073dd7474099086d43a87e8c682
Joe Zhou <chn.challenger@gmail.com>
2015-11-24 22:02:07 +0000

    latexprinter can now print individual fraction questions

--- generators/latex_printer.rb
+++ generators/latex_printer.rb
@@ -1,69 +1,56 @@
-require './generators/question'
+require './generators/fraction'
 
 class LatexPrinter
-include Questions
+# include Questions
 
-  HEADERS = '\documentclass{article}
-            \usepackage[math]{iwona}
-            \usepackage[fleqn]{amsmath}
-            \usepackage{scrextend}
-            \changefontsizes[20pt]{18pt}
-            \usepackage[margin=0.7in]{geometry}
-            \pagenumbering{gobble}
-            \usepackage{setspace}
-            \renewcommand{\baselinestretch}{1.5}
-            \begin{document}'
+  # HEADERS = '\documentclass{article}
+  #           \usepackage[math]{iwona}
+  #           \usepackage[fleqn]{amsmath}
+  #           \usepackage{scrextend}
+  #           \changefontsizes[20pt]{18pt}
+  #           \usepackage[margin=0.7in]{geometry}
+  #           \pagenumbering{gobble}
+  #           \usepackage{setspace}
+  #           \renewcommand{\baselinestretch}{1.5}
+  #           \begin{document}'
+  #
+  # TEST_QUESTIONS = '1.\hspace{10pt}&2\frac{3}{4} + 4 \frac{2}{7} \hspace{90pt} &12.\hspace{10pt} &4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
+  # 3.\hspace{10pt}&2\frac{3}{4}  + 2314 \frac{12}{17} &11.\hspace{10pt}&4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
+  # 1.\hspace{10pt}&2\frac{3}{4} + 4 \frac{2}{7} \hspace{90pt} &12.\hspace{10pt} &4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
+  # 3.\hspace{10pt}&2\frac{3}{4}  + 2314 \frac{12}{17} &11.\hspace{10pt}&4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
+  # 1.\hspace{10pt}&2\frac{3}{4} + 4 \frac{2}{7} \hspace{90pt} &12.\hspace{10pt} &4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
+  # 3.\hspace{10pt}&2\frac{3}{4}  + 2314 \frac{12}{17} &11.\hspace{10pt}&4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
+  # 3.\hspace{10pt}&2\frac{3}{4}  + 4 \frac{2}{7}  &11.&4\frac{2}{5} + 3\frac{5}{9}'
+  #
+  # def latex_worksheet(title='Worksheet 1',questions_per_row=2,number_of_rows=5,question_type=['addition'])
+  #   result = HEADERS + "\n" + '\section*{\centerline{Fraction ' + title + '}}'+ "\n\n" + '\vspace{10 mm}' + "\n" + '\begin{align*}' + "\n"
+  #   result += TEST_QUESTIONS + "\n" + '\end{align*}' + "\n" + '\end{document}'+ "\n"
+  #   result
+  # end
 
-  TEST_QUESTIONS = '1.\hspace{10pt}&2\frac{3}{4} + 4 \frac{2}{7} \hspace{90pt} &12.\hspace{10pt} &4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
-  3.\hspace{10pt}&2\frac{3}{4}  + 2314 \frac{12}{17} &11.\hspace{10pt}&4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
-  1.\hspace{10pt}&2\frac{3}{4} + 4 \frac{2}{7} \hspace{90pt} &12.\hspace{10pt} &4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
-  3.\hspace{10pt}&2\frac{3}{4}  + 2314 \frac{12}{17} &11.\hspace{10pt}&4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
-  1.\hspace{10pt}&2\frac{3}{4} + 4 \frac{2}{7} \hspace{90pt} &12.\hspace{10pt} &4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
-  3.\hspace{10pt}&2\frac{3}{4}  + 2314 \frac{12}{17} &11.\hspace{10pt}&4\frac{2}{5} + 3\frac{5}{9}\\\[2em]
-  3.\hspace{10pt}&2\frac{3}{4}  + 4 \frac{2}{7}  &11.&4\frac{2}{5} + 3\frac{5}{9}'
-
-  def latex_worksheet(title='Worksheet 1',questions_per_row=2,number_of_rows=5,question_type=['addition'])
-    result = HEADERS + "\n" + '\section*{\centerline{Fraction ' + title + '}}'+ "\n\n" + '\vspace{10 mm}' + "\n" + '\begin{align*}' + "\n"
-    result += TEST_QUESTIONS + "\n" + '\end{align*}' + "\n" + '\end{document}'+ "\n"
-    result
-  end
-
-  def latex_fraction_question(operation='multiply',integer_range=10,fraction_range=10)
-    question = fraction_question(operation,integer_range,fraction_range)
-    latex_result = {}
-    # latex_result[:question] = "#{question[:fraction1].integer}\\frac{#{question[:fraction1].numerator}}{#{question[:fraction1].denominator}}"
-    latex_result[:question] = _latex_fraction(question[:fraction1])
-    case operation
-      when 'addition' then sign = '+'
-      when 'subtract' then sign = '-'
-      when 'multiply' then sign = '\times'
-      when 'divide' then sign = '\div'
-    end
-    latex_result[:question] += sign
-    # latex_result[:question] += "#{question[:fraction2].integer}\\frac{#{question[:fraction2].numerator}}{#{question[:fraction2].denominator}}"
-    # latex_result[:solution] = "#{question[:solution].integer}\\frac{#{question[:solution].numerator}}{#{question[:solution].denominator}}"
-    latex_result[:question] += _latex_fraction(question[:fraction2])
-    latex_result[:solution] = _latex_fraction(question[:solution])
-
-    puts latex_result[:question]
-    puts latex_result[:solution]
-
-    puts ''
-    puts ''
-
-    return latex_result
+  def self.fraction_question(operation='addition',integer_range=10,fraction_range=10)
+    question = Fraction.question(operation,integer_range,fraction_range)
+    question_latex = self._latex_fraction(question[:fraction1]) +
+      self._latex_sign(operation) + self._latex_fraction(question[:fraction2])
+    solution_latex = self._latex_fraction(question[:solution])
+    {question:question_latex,solution:solution_latex}
   end
 
   private
 
-  def _latex_fraction(fraction)
-    # if fraction.integer == 0
-    #   "\\frac{#{fraction.numerator}}{#{fraction.denominator}}"
-    # else
-    #   "#{fraction.integer}\\frac{#{fraction.numerator}}{#{fraction.denominator}}"
-    # end
+  def self._latex_fraction(fraction)
     result = fraction.integer == 0 ? "" : "#{fraction.integer}"
     result += "\\frac{#{fraction.numerator}}{#{fraction.denominator}}"
+  end
+
+  def self._latex_sign(operation)
+    sign = case operation
+      when 'addition' then  '+'
+      when 'subtract' then  '-'
+      when 'multiply' then  '\times'
+      when 'divide' then '\div'
+      else '+'
+    end
   end
 
 end
@@ -112,6 +99,7 @@ end
 a = 5
 b = 6
 # puts "\\frac{#{a}}{#{b}}"
-c = LatexPrinter.new
-d = c.latex_fraction_question
-puts d
+# c = LatexPrinter.new
+d = LatexPrinter.fraction_question
+# puts d
+# puts d
--- spec/latex_printer_spec.rb
+++ spec/latex_printer_spec.rb
@@ -1,8 +1,34 @@
 require './generators/latex_printer'
 
 describe LatexPrinter do
-  describe '#latex_fraction_question' do
-    
+  describe '#fraction_question' do
+    it 'can print latex fraction addition question' do
+      srand(100)
+      latex_result = LatexPrinter.fraction_question('addition')
+      expected_result_hash = {question:"8\\frac{2}{5}+7\\frac{8}{9}",
+                              solution:"16\\frac{13}{45}"}
+      expect(latex_result).to eq expected_result_hash
+    end
 
+    it 'can print latex fraction subtraction question' do
+      srand(200)
+      latex_result = LatexPrinter.fraction_question('subtract')
+      expected_result_hash = {question:"10\\frac{1}{2}-4\\frac{5}{9}",solution:"5\\frac{17}{18}"}
+      expect(latex_result).to eq expected_result_hash
+    end
+
+    it 'can print latex fraction multiplication question' do
+      srand(300)
+      latex_result = LatexPrinter.fraction_question('multiply')
+      expected_result_hash = {question:"1\\frac{2}{3}\\times2\\frac{1}{2}", solution:"4\\frac{1}{6}"}
+      expect(latex_result).to eq expected_result_hash
+    end
+
+    it 'can print latex fraction division question' do
+      srand(400)
+      latex_result = LatexPrinter.fraction_question('divide')
+      expected_result_hash = {question:"4\\frac{1}{4}\\div4\\frac{4}{5}", solution:"\\frac{85}{96}"}
+      expect(latex_result).to eq expected_result_hash
+    end
   end
 end

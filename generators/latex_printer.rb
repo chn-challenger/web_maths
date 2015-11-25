e8574b8424d71e75247bedee011a4876ba128cd9
Joe Zhou <chn.challenger@gmail.com>
2015-11-25 17:47:03 +0000

    fraction latex sheet tests and implementation

--- generators/latex_printer.rb
+++ generators/latex_printer.rb
@@ -25,32 +25,55 @@ class LatexPrinter
 
   def self.fraction_sheet_content(questions_per_row=2,number_of_rows=5,operation=['add'],
     integer_range=10,fraction_range=10)
+
     questions = Fraction.worksheet_questions(questions_per_row * number_of_rows,
       operation,integer_range,fraction_range)
     question_number = 1
+
     latex_questions = ''
+    latex_solutions = ''
+
     for i in 1..number_of_rows
       questions_per_row.times do
         latex_questions += '&' if question_number%questions_per_row != 1
         latex_questions += "&#{question_number}.\\hspace{#{QUESTION_AND_NUMBER_SPACING}pt}"
         latex_questions += self.fraction_question(questions[question_number-1])[:question]
+
+        latex_solutions += '&' if question_number%questions_per_row != 1
+        latex_solutions += "&#{question_number}.\\hspace{#{QUESTION_AND_NUMBER_SPACING}pt}"
+        latex_solutions += self.fraction_question(questions[question_number-1])[:solution]
+
         question_number += 1
       end
       latex_questions += '\\\[2em]' if i != number_of_rows
       latex_questions += "\n"
+
+      latex_solutions += '\\\[2em]' if i != number_of_rows
+      latex_solutions += "\n"
     end
-    {questions: latex_questions}
+    {questions: latex_questions,solutions: latex_solutions}
   end
 
   def self.fraction_sheet(title='',questions_per_row=2,number_of_rows=5,operation=['add','subtract','multiply','divide'],
     integer_range=10,fraction_range=10)
+    # latex_questions_sheet = HEADERS
+    # latex_questions_sheet += "\\section*{\\centerline{Fraction Worksheet #{title}}}\n\\vspace{#{TITLE_CONTENT_SPACE} mm}\n\\begin{align*}\n"
+    # content = self.fraction_sheet_content(questions_per_row,number_of_rows,operation,integer_range,fraction_range)[:questions]
+    # latex_questions_sheet += content + "\\end{align*}\n\\end{document}"
+    # {fraction_sheet:latex_questions_sheet}
+
+    content = self.fraction_sheet_content(questions_per_row,number_of_rows,operation,integer_range,fraction_range)
 
     latex_questions_sheet = HEADERS
     latex_questions_sheet += "\\section*{\\centerline{Fraction Worksheet #{title}}}\n\\vspace{#{TITLE_CONTENT_SPACE} mm}\n\\begin{align*}\n"
-    content = self.fraction_sheet_content(questions_per_row,number_of_rows,operation,integer_range,fraction_range)[:questions]
-    latex_questions_sheet += content + "\\end{align*}\n\\end{document}"
+    latex_questions_sheet += content[:questions] + "\\end{align*}\n\\end{document}"
 
-    {fraction_sheet:latex_questions_sheet}
+    latex_solutions_sheet = HEADERS
+    latex_solutions_sheet += "\\section*{\\centerline{Fraction Worksheet #{title} Solutions}}\n\\vspace{#{TITLE_CONTENT_SPACE} mm}\n\\begin{align*}\n"
+    latex_solutions_sheet += content[:solutions] + "\\end{align*}\n\\end{document}"
+
+    {questions_sheet:latex_questions_sheet,solutions_sheet:latex_solutions_sheet}
+
   end
 
   private
@@ -72,5 +95,5 @@ class LatexPrinter
 
 end
 
-test = LatexPrinter.fraction_sheet('Test',3,6)[:fraction_sheet]
-puts test
+# test = LatexPrinter.fraction_sheet('Test',3,6)[:fraction_sheet]
+# puts test
--- spec/latex_printer_spec.rb
+++ spec/latex_printer_spec.rb
@@ -37,29 +37,116 @@ describe LatexPrinter do
   end
 
   describe '#fraction_questionsheet_content' do
-    it 'can generate one rows of fraction questions' do
-      srand(100)
-      questions = LatexPrinter.fraction_sheet_content(3,1)[:questions]
-      expected_questions =  "1.\\hspace{15pt}&8\\frac{2}{5}+7\\frac{8}{9}&2.\\"\
-        "hspace{15pt}&\\frac{1}{2}+5\\frac{3}{4}&3.\\hspace{15pt}&2\\frac{1}{3"\
-        "}+8\\frac{2}{3}\n"
-      expect(questions).to eq expected_questions
+    context 'can generate one row of questions and answers' do
+      shared_context 'random sheet with one row of questions' do
+        before(:all) do
+          srand(100)
+          @sheet = LatexPrinter.fraction_sheet_content(3,1)
+        end
+      end
+
+      include_context 'random sheet with one row of questions'
+
+      it 'can generate one rows of fraction questions' do
+        questions = @sheet[:questions]
+        expected_questions = "&1.\\hspace{15pt}8\\frac{2}{5}+7\\frac{8}{9}&&2.\\"\
+          "hspace{15pt}\\frac{1}{2}+5\\frac{3}{4}&&3.\\hspace{15pt}2\\frac{1}{3"\
+          "}+8\\frac{2}{3}\n"
+        expect(questions).to eq expected_questions
+      end
+
+      it 'can generate corresponding solutions' do
+        solutions = @sheet[:solutions]
+        expected_solutions = "&1.\\hspace{15pt}16\\frac{13}{45}&&2.\\hspace{15"\
+          "pt}6\\frac{1}{4}&&3.\\hspace{15pt}11\\frac{0}{1}\n"
+        expect(solutions).to eq expected_solutions
+      end
+
     end
-    it 'can generate six rows of fraction questions' do
-      srand(200)
-      questions = LatexPrinter.fraction_sheet_content(2,6,['add','subtract',
-        'multiply','divide'])[:questions]
-      expected_questions =  "1.\\hspace{15pt}&1\\frac{1}{2}\\times4\\frac{1}{4"\
-        "}&2.\\hspace{15pt}&1\\frac{1}{2}\\div1\\frac{2}{3}\\\\[2em]\n3.\\hspa"\
-        "ce{15pt}&2\\frac{1}{2}\\times1\\frac{4}{5}&4.\\hspace{15pt}&1\\frac{2"\
-        "}{3}-1\\frac{1}{10}\\\\[2em]\n5.\\hspace{15pt}&10\\frac{7}{8}+3\\frac"\
-        "{5}{6}&6.\\hspace{15pt}&6\\frac{1}{4}\\times3\\frac{1}{6}\\\\[2em]\n7"\
-        ".\\hspace{15pt}&4\\frac{3}{4}-1\\frac{2}{3}&8.\\hspace{15pt}&\\frac{3"\
-        "}{7}\\times6\\frac{4}{7}\\\\[2em]\n9.\\hspace{15pt}&4\\frac{3}{5}\\ti"\
-        "mes6\\frac{2}{3}&10.\\hspace{15pt}&1\\frac{2}{7}\\div\\frac{1}{5}\\\\"\
-        "[2em]\n11.\\hspace{15pt}&\\frac{2}{3}\\div5\\frac{7}{8}&12.\\hspace{1"\
-        "5pt}&7\\frac{2}{3}+4\\frac{1}{3}\n"
-      expect(questions).to eq expected_questions
+
+    context 'can generate six rows of questions and answers' do
+      shared_context 'random sheet with six rows of questions' do
+        before(:all) do
+          srand(200)
+          @sheet = LatexPrinter.fraction_sheet_content(2,6,['add','subtract',
+            'multiply','divide'])
+        end
+      end
+
+      include_context 'random sheet with six rows of questions'
+
+      it 'can generate six rows of fraction questions' do
+        questions = @sheet[:questions]
+        expected_questions =  "&1.\\hspace{15pt}1\\frac{1}{2}\\times4\\frac{1}{4"\
+          "}&&2.\\hspace{15pt}1\\frac{1}{2}\\div1\\frac{2}{3}\\\\[2em]\n&3.\\hsp"\
+          "ace{15pt}2\\frac{1}{2}\\times1\\frac{4}{5}&&4.\\hspace{15pt}1\\frac{2"\
+          "}{3}-1\\frac{1}{10}\\\\[2em]\n&5.\\hspace{15pt}10\\frac{7}{8}+3\\frac"\
+          "{5}{6}&&6.\\hspace{15pt}6\\frac{1}{4}\\times3\\frac{1}{6}\\\\[2em]\n&"\
+          "7.\\hspace{15pt}4\\frac{3}{4}-1\\frac{2}{3}&&8.\\hspace{15pt}\\frac{3"\
+          "}{7}\\times6\\frac{4}{7}\\\\[2em]\n&9.\\hspace{15pt}4\\frac{3}{5}\\ti"\
+          "mes6\\frac{2}{3}&&10.\\hspace{15pt}1\\frac{2}{7}\\div\\frac{1}{5}\\\\"\
+          "[2em]\n&11.\\hspace{15pt}\\frac{2}{3}\\div5\\frac{7}{8}&&12.\\hspace{"\
+          "15pt}7\\frac{2}{3}+4\\frac{1}{3}\n"
+        expect(questions).to eq expected_questions
+      end
+
+      it 'can generate corresponding solutions' do
+        solutions = @sheet[:solutions]
+        expected_solutions = "&1.\\hspace{15pt}6\\frac{3}{8}&&2.\\hspace{15pt}"\
+          "\\frac{9}{10}\\\\[2em]\n&3.\\hspace{15pt}4\\frac{1}{2}&&4.\\hspace{"\
+          "15pt}\\frac{17}{30}\\\\[2em]\n&5.\\hspace{15pt}14\\frac{17}{24}&&6."\
+          "\\hspace{15pt}19\\frac{19}{24}\\\\[2em]\n&7.\\hspace{15pt}3\\frac{1"\
+          "}{12}&&8.\\hspace{15pt}2\\frac{40}{49}\\\\[2em]\n&9.\\hspace{15pt}3"\
+          "0\\frac{2}{3}&&10.\\hspace{15pt}6\\frac{3}{7}\\\\[2em]\n&11.\\hspac"\
+          "e{15pt}\\frac{16}{141}&&12.\\hspace{15pt}12\\frac{0}{1}\n"
+        expect(solutions).to eq expected_solutions
+      end
     end
   end
+
+  describe '#fraction_sheet' do
+    shared_context 'generate a random fraction sheet and answers' do
+      before(:all) do
+        srand(200)
+        @sheet = LatexPrinter.fraction_sheet('Test')
+      end
+    end
+
+    include_context 'generate a random fraction sheet and answers'
+
+    it 'can generate a random latex fractions sheet' do
+      expected_sheet = "\\documentclass{article}\n\\usepackage[math]{iwona}\n"\
+        "\\usepackage[fleqn]{amsmath}\n\\usepackage{scrextend}\n\\changefontsi"\
+        "zes[20pt]{17pt}\n\\usepackage[margin=0.7in]{geometry}\n\\pagenumberin"\
+        "g{gobble}\n\\begin{document}\n\\section*{\\centerline{Fraction Worksh"\
+        "eet Test}}\n\\vspace{10 mm}\n\\begin{align*}\n&1.\\hspace{15pt}1\\fra"\
+        "c{1}{2}\\times4\\frac{1}{4}&&2.\\hspace{15pt}1\\frac{1}{2}\\div1\\fra"\
+        "c{2}{3}\\\\[2em]\n&3.\\hspace{15pt}2\\frac{1}{2}\\times1\\frac{4}{5}&"\
+        "&4.\\hspace{15pt}1\\frac{2}{3}-1\\frac{1}{10}\\\\[2em]\n&5.\\hspace{1"\
+        "5pt}10\\frac{7}{8}+3\\frac{5}{6}&&6.\\hspace{15pt}6\\frac{1}{4}\\time"\
+        "s3\\frac{1}{6}\\\\[2em]\n&7.\\hspace{15pt}4\\frac{3}{4}-1\\frac{2}{3}"\
+        "&&8.\\hspace{15pt}\\frac{3}{7}\\times6\\frac{4}{7}\\\\[2em]\n&9.\\hsp"\
+        "ace{15pt}4\\frac{3}{5}\\times6\\frac{2}{3}&&10.\\hspace{15pt}1\\frac{"\
+        "2}{7}\\div\\frac{1}{5}\n\\end{align*}\n\\end{document}"
+        puts @sheet[:questions_sheet]
+      expect(@sheet[:questions_sheet]).to eq expected_sheet
+    end
+
+    it 'can generate the corresponding solution sheet' do
+      expected_solutions = "\\documentclass{article}\n\\usepackage[math]{iwona"\
+      "}\n\\usepackage[fleqn]{amsmath}\n\\usepackage{scrextend}\n\\changefonts"\
+      "izes[20pt]{17pt}\n\\usepackage[margin=0.7in]{geometry}\n\\pagenumbering"\
+      "{gobble}\n\\begin{document}\n\\section*{\\centerline{Fraction Worksheet"\
+      " Test Solutions}}\n\\vspace{10 mm}\n\\begin{align*}\n&1.\\hspace{15pt}6"\
+      "\\frac{3}{8}&&2.\\hspace{15pt}\\frac{9}{10}\\\\[2em]\n&3.\\hspace{15pt}"\
+      "4\\frac{1}{2}&&4.\\hspace{15pt}\\frac{17}{30}\\\\[2em]\n&5.\\hspace{15p"\
+      "t}14\\frac{17}{24}&&6.\\hspace{15pt}19\\frac{19}{24}\\\\[2em]\n&7.\\hsp"\
+      "ace{15pt}3\\frac{1}{12}&&8.\\hspace{15pt}2\\frac{40}{49}\\\\[2em]\n&9."\
+      "\\hspace{15pt}30\\frac{2}{3}&&10.\\hspace{15pt}6\\frac{3}{7}\n\\end{ali"\
+      "gn*}\n\\end{document}"
+      expect(@sheet[:solutions_sheet]).to eq expected_solutions
+    end
+
+  end
+
 end

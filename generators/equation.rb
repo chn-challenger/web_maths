9937828251221bd4d59412657332286b87e9b088
Joe Zhou <chn.challenger@gmail.com>
2015-11-27 18:13:04 +0000

    can calculte solution next pair of steps

--- generators/equation.rb
+++ generators/equation.rb
@@ -14,6 +14,40 @@ class Equation
     left_side == equation.left_side && right_side == equation.right_side
   end
 
+  def solution_next_step
+    self_copy = self.dup
+    if self_copy.left_side.steps.count > 0
+      steps_side = self_copy.left_side
+      values_side = self_copy.right_side
+    else
+      steps_side = self_copy.right_side
+      values_side = self_copy.left_side
+    end
+    if steps_side.steps.count > 0
+      last_step = steps_side.steps.shift
+
+      side_swap = true if last_step.orientation == :left && (last_step.operation == :divide || last_step.operation == :subtract )
+
+      reverse_of_last_step = last_step.reverse
+      values_side.steps << reverse_of_last_step
+    end
+    self_copy
+    
+    self_copy_copy = self_copy.dup
+    if self_copy_copy.left_side.initial_value.is_a?(Integer)
+      self_copy_copy.left_side = self_copy_copy.left_side.evaluate_next_step
+    else
+      self_copy_copy.right_side = self_copy_copy.right_side.evaluate_next_step
+    end
+
+    if !!side_swap
+      self_copy.left_side, self_copy.right_side = self_copy.right_side, self_copy.left_side
+      self_copy_copy.left_side, self_copy_copy.right_side = self_copy_copy.right_side, self_copy_copy.left_side
+    end
+
+    [self_copy,self_copy_copy]
+  end
+
   def generate_solution
     solution_equations = []
     first_equation = self.dup
@@ -21,8 +55,6 @@ class Equation
     second_equation = first_equation.dup
     step = second_equation.left_side.steps.shift
     step.reverse
-
-
   end
 
 end
--- spec/equation_spec.rb
+++ spec/equation_spec.rb
@@ -17,6 +17,79 @@ describe Equation do
     end
   end
 
+  describe '#solution_next_step' do
+    it 'returns next pair of steps one step right addition equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:add,5,:right)]),Expression.new(11))
+      expected_returns = [Equation.new(Expression.new('x'),
+        Expression.new(11,[EquationStep.new(:subtract,5,:right)])),
+        Equation.new(Expression.new('x'),Expression.new(6))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step left addition equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:add,5,:left)]),Expression.new(11))
+      expected_returns = [Equation.new(Expression.new('x'),
+        Expression.new(11,[EquationStep.new(:subtract,5,:right)])),
+        Equation.new(Expression.new('x'),Expression.new(6))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step right multiplication equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:multiply,5,:right)]),Expression.new(15))
+      expected_returns = [Equation.new(Expression.new('x'),
+        Expression.new(15,[EquationStep.new(:divide,5,:right)])),
+        Equation.new(Expression.new('x'),Expression.new(3))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step left multiplication equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:multiply,5,:left)]),Expression.new(15))
+      expected_returns = [Equation.new(Expression.new('x'),
+        Expression.new(15,[EquationStep.new(:divide,5,:right)])),
+        Equation.new(Expression.new('x'),Expression.new(3))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step right subtraction equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:subtract,5,:right)]),Expression.new(15))
+      expected_returns = [Equation.new(Expression.new('x'),
+        Expression.new(15,[EquationStep.new(:add,5,:right)])),
+        Equation.new(Expression.new('x'),Expression.new(20))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step right division equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:divide,5,:right)]),Expression.new(15))
+      expected_returns = [Equation.new(Expression.new('x'),
+        Expression.new(15,[EquationStep.new(:multiply,5,:right)])),
+        Equation.new(Expression.new('x'),Expression.new(75))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step left subtraction equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:subtract,21,:left)]),Expression.new(11))
+      expected_returns = [Equation.new(Expression.new(11,[EquationStep.new(:subtract,21,:left)]),Expression.new('x')),
+        Equation.new(Expression.new(10),Expression.new('x'))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+    it 'returns next pair of steps one step left division equation' do
+      equation = Equation.new(Expression.new('x',
+        [EquationStep.new(:divide,21,:left)]),Expression.new(7))
+      expected_returns = [Equation.new(Expression.new(7,[EquationStep.new(:divide,21,:left)]),Expression.new('x')),
+        Equation.new(Expression.new(3),Expression.new('x'))]
+      expect(equation.solution_next_step).to eq expected_returns
+    end
+
+  end
+
   # describe '#generate_solution' do
   #   it 'for one step right addition equation' do
   #     step1 = EquationStep.new(:add,5,:right)
